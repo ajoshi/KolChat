445c50b6ceea88b1260d9dac8eb4ef39ed7793a9
ajoshi <ayj5012@gmail.com>
2017-08-21 22:25:06 -0700

    Fixing chat order

--- app/src/main/java/biz/ajoshi/kolchat/persistence/MessageDao.java
+++ app/src/main/java/biz/ajoshi/kolchat/persistence/MessageDao.java
@@ -19,11 +19,11 @@ public interface MessageDao {
     List<ChatMessage> getMessages();
 
     // gets next pages. Offset might need to depend on hwo many items are shown (db could have more inserted in the meantime)
-    @Query("SELECT * FROM chatmessage WHERE channelId = :channel_id ORDER BY timeStamp ASC LIMIT 100 OFFSET :offset")
+    @Query("SELECT * FROM (SELECT * FROM chatmessage WHERE channelId = :channel_id ORDER BY timeStamp DESC LIMIT 100 OFFSET :offset) ORDER BY timestamp ASC")
     List<ChatMessage> getMessagesForChannel (String channel_id, int offset);
 
-    // gets first page of messages
-    @Query("SELECT * FROM chatmessage WHERE channelId = :channel_id ORDER BY timeStamp ASC LIMIT 100")
+    // gets 100 most recent messages, with the newest one at the bottom
+    @Query("SELECT * FROM (SELECT * FROM chatmessage WHERE channelId = :channel_id ORDER BY timeStamp DESC LIMIT 100) ORDER BY timestamp ASC")
     List<ChatMessage> getMessagesForChannel (String channel_id);
 
     @Query("SELECT * FROM chatmessage WHERE channelId = :channel_id ORDER BY timeStamp ASC LIMIT 1")
