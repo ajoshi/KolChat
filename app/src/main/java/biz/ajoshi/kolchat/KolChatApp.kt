b2475516d18728c8084ae0ed71c458a053ec718a
ajoshi <ayj5012@gmail.com>
2018-06-01 21:17:33 -0700

    Using Send Logs pref to decide if we log to crashlytics or not

--- app/src/main/java/biz/ajoshi/kolchat/KolChatApp.kt
+++ app/src/main/java/biz/ajoshi/kolchat/KolChatApp.kt
@@ -2,6 +2,7 @@ package biz.ajoshi.kolchat
 
 import android.app.Application
 import android.os.StrictMode
+import android.support.v7.preference.PreferenceManager
 import biz.ajoshi.commonutils.Logg
 import biz.ajoshi.kolchat.chat.ChatJob
 import biz.ajoshi.kolchat.persistence.KolDB
@@ -18,7 +19,13 @@ class KolChatApp : Application() {
         super.onCreate()
         // migrations are probably dags to allow db upgrades from one version to another?
         Fabric.with(this, Crashlytics())
-        Logg.setCustomLogger(CrashlyticsLogger())
+        val preferenceManager = PreferenceManager.getDefaultSharedPreferences(this)
+
+        // set the custom logger to log to Crashlytics if the user has allowed it.
+        val shouldSendLogs = preferenceManager.getBoolean(KEY_PREF_SEND_LOGS, true)
+        if (shouldSendLogs) {
+            Logg.setCustomLogger(CrashlyticsLogger())
+        }
         KolDB.createDb(this)
         Fresco.initialize(this)
         StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().detectAll().build())
